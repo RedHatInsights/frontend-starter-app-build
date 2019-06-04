1a436925bd52062b9e755d50df8606f3d372bd3b
Travis CI User <travis@example.org>
2019-06-04 13:53:50 +0000

    Use simplified config from FEC monorepo (#71)
    * Use simplified config from FEC monorepo
    
    * Add postcss to allow style linting
    
    * Include css loader to fix build

---
+++ js/Rules.js
@@ -0,0 +1,30 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["Rules"],{
+
+/***/ "./src/PresentationalComponents/Rules/ListRules.js":
+/*!*********************************************************!*\
+  !*** ./src/PresentationalComponents/Rules/ListRules.js ***!
+  \*********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _redhat_cloud_services_frontend_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @redhat-cloud-services/frontend-components */ "./node_modules/@redhat-cloud-services/frontend-components/index.js");
+/* harmony import */ var _redhat_cloud_services_frontend_components__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_redhat_cloud_services_frontend_components__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+var ListRules = function ListRules() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_redhat_cloud_services_frontend_components__WEBPACK_IMPORTED_MODULE_1__["PageHeader"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_redhat_cloud_services_frontend_components__WEBPACK_IMPORTED_MODULE_1__["PageHeaderTitle"], {
+    title: "Rules Page"
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, " The is the route to the rules page ")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_redhat_cloud_services_frontend_components__WEBPACK_IMPORTED_MODULE_1__["Main"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, " Content ")));
+};
+
+ListRules.displayName = 'list-rules';
+/* harmony default export */ __webpack_exports__["default"] = (ListRules);
+
+/***/ })
+
+}]);
