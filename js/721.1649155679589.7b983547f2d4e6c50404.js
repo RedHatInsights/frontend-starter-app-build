19fdccfa59424916182788225fd79af81e441479
Travis CI User <travis@example.org>
2022-04-05 10:48:41 +0000

    Bump nth-check from 2.0.0 to 2.0.1
    Bumps [nth-check](https://github.com/fb55/nth-check) from 2.0.0 to 2.0.1.
    - [Release notes](https://github.com/fb55/nth-check/releases)
    - [Commits](https://github.com/fb55/nth-check/compare/v2.0.0...v2.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: nth-check
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/721.1649155679589.7b983547f2d4e6c50404.js
+++ js/721.1649155679589.7b983547f2d4e6c50404.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkfrontend_starter_app=self.webpackChunkfrontend_starter_app||[]).push([[721],{6288:(e,n,a)=>{a.r(n),a.d(n,{default:()=>c});var p=a(5697),r=a.n(p),t=a(5418),s=a.n(t),o=function(e){return s().createElement("span",{className:"sample-component"}," ",e.children," ")};o.displayName="SampleComponent",o.propTypes={children:r().oneOfType([r().arrayOf(r().node),r().node])};const c=o}}]);
-//# sourceMappingURL=../sourcemaps/721.6f9d3c957c8e5fcbff3f595f84177690.js.map
+//# sourceMappingURL=../sourcemaps/721.4a707c60221db790d05abd02208c71d4.js.map
